a14d6310806e5a36ae91856e1945f8bf4a6effe4
kasxrocket <bob.geek.nz@protonmail.com>
2024-11-14 23:28:49 +1000

    Minor Update

---
+++ public/includes/scripts/kas-fee.js
@@ -0,0 +1,19 @@
+// Define the API endpoint
+const url = "https://api.kasx.io/info/fee-estimate";
+
+// Make the GET request
+fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log("Fee Estimate:", data);
+    // Display the fee estimate in the web page
+    document.getElementById("fee-estimate").innerText = JSON.stringify(data);
+  })
+  .catch(error => {
+    console.error("Error fetching fee estimate:", error);
+  });
